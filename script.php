94fc3131b8d36984a055abc44c36654905945b5c
Julian Zuluaga <zuliandres@gmail.com>
2017-04-01 09:48:01 -0500

    Update script.php

--- script.php
+++ script.php
@@ -1,4 +1,3 @@
-
 <?php 
 
 if(isset($_POST['submit'])){
@@ -17,13 +16,6 @@ else{
 }
 }
 
-
-
-//$object = $_POST["object"];
-//$location = $_POST["location"];
-//$time = $_POST["days"];
-
-//$requested = $_POST["request"];
 
 
 $user = 'root';
@@ -45,21 +37,9 @@ $success = mysqli_real_connect(
 
 
 //Create connection
-mysql_connect(localhost:8889,$user,$password);
+mysql_connect($host,$user,$password);
 mysql_select_db($db) or die( "Unable to select database");
 
-//$query = "INSERT INTO contacts VALUES ('','$first','$last','$phone','$mobile','$fax','$email','$web')";
-//mysql_query($query);
-
-
-
-//$NeederData = $conn->prepare("select location, itemNeeded from Needer");
-//$GiverData = $conn->prepare("select location, itemGiving from Giver");
-
-//if ($NeederData === $GiverData) {
-	
-	//no idea to do for when they match
-//}
 
 
 if ($requested == true){
@@ -70,12 +50,4 @@ else
 	$sql = "INSERT INTO Given(name, location, days)
 	VALUES ('','$object', '$location', '$days');";
 
-if ($conn->multi_query($sql) === TRUE) {
-    echo "New records created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-
-$conn->close();
 ?>
